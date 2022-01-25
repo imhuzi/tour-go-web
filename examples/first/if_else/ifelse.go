0f16fffeb9788ffe180e29a808ffd17fe3a0fbe3
Huzi.Wang <huzi.wh@gmail.com>
2022-01-25 23:50:38 +0800

    if else

--- examples/first/if_else/ifelse.go
+++ examples/first/if_else/ifelse.go
@@ -1,6 +1,17 @@
 package main
 
 func main() {
+	r := testIfesle(10)
+	println("r: ", r)
+}
 
-
+/**
+test if-else , if-else 后面的 {} 是强制的
+*/
+func testIfesle(x int) (b int) {
+	if x > 0 { // if 开始的 {  必须 和 if在同一行
+		return x
+	} else {
+		return 0
+	}
 }
