0d22b4bbcd237fef3117ca1957de8e34dbdbfa28
Huzi.Wang <huzi.wh@gmail.com>
2022-01-28 11:59:31 +0800

    func test

--- examples/first/array_and_slice/slice.go
+++ examples/first/array_and_slice/slice.go
@@ -4,23 +4,22 @@ import "fmt"
 
 func main() {
 	// 直接初始化了 3个元素的切片
-	s1 := []int{1,2,3}
+	s1 := []int{1, 2, 3}
 	fmt.Printf("s1: %v, len: %d, cap: %d \n", s1, len(s1), cap(s1))
-
 	// 创建了包含3个元素，容量为4的切片
-	s2 := make([]int,3,4)
+	s2 := make([]int, 3, 4)
 	fmt.Printf("s2: %v, len: %d, cap: %d \n", s2, len(s2), cap(s2))
 	// s2 目前 是[0,0,0]  append (追加) 一个元素， 变成什么？
-	s2 = append(s2,7) // 没有超出容量限制，不会发生扩容
+	s2 = append(s2, 7) // 没有超出容量限制，不会发生扩容
 	// [0,0,0,7]
 	fmt.Printf("s2: %v, len: %d, cap: %d \n", s2, len(s2), cap(s2))
 
 	// 在追加一个 元素, 触发扩容, 扩容策略？
-	s2 = append(s2,8)
+	s2 = append(s2, 8)
 	fmt.Printf("s2: %v, len: %d, cap: %d \n", s2, len(s2), cap(s2))
 
 	// 只传一个参数 表示 创建一个含有4个元素，容量也是4的切片
-	s3 := make([]int,4)
+	s3 := make([]int, 4)
 	// 等价于 s3 := make([]int,4,4)
 	fmt.Printf("s3: %v, len: %d, cap: %d \n", s3, len(s3), cap(s3))
 }
--- examples/first/func_dec/funcs.go
+++ examples/first/func_dec/funcs.go
@@ -1,1 +1,48 @@
-package func_dec
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(div(10, 3))
+	fmt.Println(div(14, 3))
+	fmt.Println(eval(3, 5, "*"))
+
+	if result, err := eval(3, 5, "x"); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("result: ", result)
+	}
+}
+
+func eval(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a + b, nil
+	case "*":
+		return a + b, nil
+	case "/":
+		// 忽略调 余数
+		a, _ := div(a, b)
+		return a, nil
+	default:
+		return 0, fmt.Errorf("Not Support Operate: %s", op)
+	}
+}
+
+/**
+带余除法: 多个返回参数
+*/
+func div(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+/**
+  多个返回参数命名
+*/
+func div2(a, b int) (q int, r int) {
+	q = a / b
+	r = a % b
+	return
+}
